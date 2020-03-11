1769b30e9b72d93e1c4ec1d757d5c9e79c5b4be0
alxanderpierre <alxanderpierre@users.noreply.github.com>
2020-03-11 12:35:21 -0400

    new update

--- src/14_cal.py
+++ src/14_cal.py
@@ -2,7 +2,7 @@
 The Python standard library's 'calendar' module allows you to
 render a calendar to your terminal.
 https://docs.python.org/3.6/library/calendar.html
-
+​
 Write a program that accepts user input of the form
   `14_cal.py [month] [year]`
 and does the following:
@@ -17,21 +17,54 @@ and does the following:
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
-
+​
 Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
-
+​
 This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
 print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
-
+​
 import sys
 import calendar
 from datetime import datetime
-
-year = int(input('Enter the Year: '))
-month = int(input('Enter the Month: '))
-
-print(calendar.month(year,month))
+​
+​
+# 14_cal.py [month] [year]
+​
+args = sys.argv
+​
+# Read the argument values
+​
+# Check if they are valid
+​
+# Default to current month/year
+month = datetime.now().month
+year = datetime.now().year
+​
+# 1 arg should set the month
+if len(args) == 1:
+    pass
+elif len(args) == 2:
+    month = int(args[1])
+    # print error message if not 1-12
+# 2 args should set the month and year
+elif len(args) == 3:
+    month = int(args[1])
+    year = int(args[2])
+# Otherwise, print error and usage message
+else:
+    print("ERROR: Should be in format '14_cal.py [month] [year]'")
+    exit(0)
+​
+if month < 1 or month > 12:
+    print("ERROR: Invalid month")
+    exit(0)
+​
+​
+tc = calendar.TextCalendar()
+​
+# Print calendar for given month and year
+tc.prmonth(year, month)
